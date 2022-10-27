b98c35b684bf7a7d753c73d67b943ec38610bbdb
Luis <lvas248@gmail.com>
2022-10-27 18:26:39 -0400

    add Create and Read capabilities to workout routes

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -1,4 +1,20 @@
 class WorkoutController < ApplicationController
 
+    get "/workouts" do 
+        Workout.all.to_json
+    end
+
+    post "/workouts" do
+        new_workout = Workout.create(
+            user_id: params[:user_id],
+            exercise_id: params[:exercise_id],
+            set_num: params[:set_num],
+            weight: params[:weight],
+            reps: params[:reps]
+        )
+        new_workout.to_json
+
+    end
+
 
 end
