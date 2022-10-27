b482e5e0fa1b9075cd5ada9d94e50b15e5e9ead8
Luis <lvas248@gmail.com>
2022-10-27 18:06:25 -0400

    add Create and Read capabilities to Exercise routes

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -1,3 +1,11 @@
 class ExerciseController < ApplicationController
 
+    get "/exercises" do
+        Exercise.all.to_json
+    end
+
+    post "/exercises" do
+        exercise = Exercise.create(exercise_name: params[:exercise_name])
+        exercise.to_json
+    end
 end
