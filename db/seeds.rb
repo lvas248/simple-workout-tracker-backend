20baffe8b99e66ba42008b26fbe4c1aef120bfb4
Luis <lvas248@gmail.com>
2022-10-27 17:41:17 -0400

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+["Luis", "Sasha", "Chimi"].each do |name|
+    User.create(user_name: name)
+end
+
+['Push Ups', 'Pull Ups', 'Sit ups', 'Military Press', 'Bench Press', 'Incline Bench Press', 'Lat Pulldown'].each do |exer|
+    Exercise.create(exercise_name: exer)
+end
 
 puts "✅ Done seeding!"
