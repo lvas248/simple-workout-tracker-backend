7d53366bb1019b0a4651e03fbf674ad2d6f8c586
Luis <lvas248@gmail.com>
2022-10-27 17:38:43 -0400

    create table for Workout migration

--- db/migrate/20221027213023_create_workouts.rb
+++ db/migrate/20221027213023_create_workouts.rb
@@ -1,4 +1,12 @@
 class CreateWorkouts < ActiveRecord::Migration[6.1]
   def change
+    create_table :workouts do |t|
+      t.integer :user_id
+      t.integer :exercise_id
+      t.integer :set_num
+      t.integer :weight
+      t.integer :reps
+      t.timestamps
+    end
   end
 end
