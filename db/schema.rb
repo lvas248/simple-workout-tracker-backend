4b0c37259fc81e2680dd9d1c62582f3a37ee4511
Luis <lvas248@gmail.com>
2023-07-12 16:27:36 -0400

    change db adapter to postres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_11_21_193221) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercises", force: :cascade do |t|
     t.string "exercise_name"
   end
